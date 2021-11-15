cf1b04273a94cf73b843af1ef073d8fae6f9a170
Martin <marnor83@gmail.com>
2021-11-15 17:09:42 +0100

    Models readded from old project

--- MathexGaming/MathexGaming/Controllers/HomeController.cs
+++ MathexGaming/MathexGaming/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace MathexGaming.Controllers
 			return View();
 		}
 
+		public IActionResult NotAuthorized()
+		{
+			return View();
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{

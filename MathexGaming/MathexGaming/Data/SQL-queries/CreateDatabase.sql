841a6fa00282881bff1be23b8c243675d08dba04
Martin <marnor83@gmail.com>
2022-01-18 16:54:59 +0100

    remove project

--- MathexGaming/MathexGaming/Data/SQL-queries/CreateDatabase.sql
+++
@@ -1,8 +0,0 @@
-USE master
-GO
-CREATE DATABASE [MathexGaming]
- ON  PRIMARY 
-( NAME = N'MathexGaming', FILENAME = N'C:\Databases\MathexGaming\MathexGaming.mdf' , SIZE = 16384KB , FILEGROWTH = 16384KB )
- LOG ON 
-( NAME = N'MathexGaming_log', FILENAME = N'C:\Databases\MathexGaming\MathexGaming_log.ldf' , SIZE = 8192KB , FILEGROWTH = 16384KB )
-GO
